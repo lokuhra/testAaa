837d55f977349f680deffb61ddfa3d8390e9b5b1
Leandro <leandro.bernardi@hipcamglobal.com>
2020-09-29 15:28:23 -0300

    a

--- packages/components/lib/modal/index.js
+++ packages/components/lib/modal/index.js
@@ -1,13 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import {
-  Box,
-  Modal,
-  Backdrop,
-  Button,
-  Typography,
-} from '@material-ui/core';
-import './styles.scss';
+import { Box, Modal, Backdrop, Button, Typography } from "@material-ui/core";
+import "./styles.scss";
 
 function ModalTC({ showTC, handleDecline, handleAccept }) {
   const [modal, setModal] = useState(false);
@@ -18,8 +12,8 @@ function ModalTC({ showTC, handleDecline, handleAccept }) {
 
   const body = (
     <Box
-      width={'100%'}
-      height={'100%'}
+      width={"100%"}
+      height={"100%"}
       display="flex"
       justifyContent="center"
       alignItems="center"
@@ -56,34 +50,30 @@ function ModalTC({ showTC, handleDecline, handleAccept }) {
           alignItems="center"
           width="90%"
           height="80%"
-          className={'container-terms'}
+          className={"container-terms"}
         >
           <p className="terms">
-            Nuestros Términos de Servicio se actualizaron el 01 de
-            Abril de 2018. Hipcam Global LLC y sus filiales
-            (colectivamente, "Hipcam") proporcionan: (1) el sitio web
-            de Hipcam ubicado en www.hipcam.com y todos los sitios
-            asociados vinculados con www.hipcam.com, incluidos el
-            sitio web de la cuenta de usuario de Hipcam al que se
-            puede acceder desde web.hipcam.com y el sitio web de
-            Hipcam al que se puede acceder desde www.hipcam.com (cada
-            uno, un "Sitio"), (2) servicios accesibles a través de los
-            Sitios ("Aplicaciones web"), (3) software que puede
-            descargarse en su smartphone o tableta para acceder a los
-            servicios ("Aplicaciones móviles"), y (4) servicios de
-            suscripción, incluidos aquellos a los que se puede acceder
-            mediante las Aplicaciones web y las Aplicaciones móviles
-            ("Servicios de suscripción"), todo ello para ser utilizado
-            junto con los productos de hardware de Hipcam
-            ("Productos") y de otras formas dispuestas por Hipcam.
-            Algunos Productos y Servicios de Hipcam pueden utilizarse
-            juntos o de forma que se integren con productos y
-            servicios de terceros. El término "Servicios" se refiere a
-            Sitios, Aplicaciones web, Aplicaciones móviles y Servicios
-            de suscripción. Algunos Productos y Servicios de Hipcam
-            pueden utilizarse juntos o de forma que se integren con
-            productos y servicios de terceros. El término "Servicios"
-            se refiere a Sitios, Aplicaciones web, Aplicaciones
+            Nuestros Términos de Servicio se actualizaron el 01 de Abril de
+            2018. Hipcam Global LLC y sus filiales (colectivamente, "Hipcam")
+            proporcionan: (1) el sitio web de Hipcam ubicado en www.hipcam.com y
+            todos los sitios asociados vinculados con www.hipcam.com, incluidos
+            el sitio web de la cuenta de usuario de Hipcam al que se puede
+            acceder desde web.hipcam.com y el sitio web de Hipcam al que se
+            puede acceder desde www.hipcam.com (cada uno, un "Sitio"), (2)
+            servicios accesibles a través de los Sitios ("Aplicaciones web"),
+            (3) software que puede descargarse en su smartphone o tableta para
+            acceder a los servicios ("Aplicaciones móviles"), y (4) servicios de
+            suscripción, incluidos aquellos a los que se puede acceder mediante
+            las Aplicaciones web y las Aplicaciones móviles ("Servicios de
+            suscripción"), todo ello para ser utilizado junto con los productos
+            de hardware de Hipcam ("Productos") y de otras formas dispuestas por
+            Hipcam. Algunos Productos y Servicios de Hipcam pueden utilizarse
+            juntos o de forma que se integren con productos y servicios de
+            terceros. El término "Servicios" se refiere a Sitios, Aplicaciones
+            web, Aplicaciones móviles y Servicios de suscripción. Algunos
+            Productos y Servicios de Hipcam pueden utilizarse juntos o de forma
+            que se integren con productos y servicios de terceros. El término
+            "Servicios" se refiere a Sitios, Aplicaciones web, Aplicaciones
             móviles y Servicios de suscripción.
           </p>
         </Box>
@@ -132,4 +122,4 @@ function ModalTC({ showTC, handleDecline, handleAccept }) {
   );
 }
 
-export { ModalTC };
+export default ModalTC;
